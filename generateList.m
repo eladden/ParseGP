57ac47ef9845287802a6d6708854ebfe92caa5bd
Elad Denenberg <eladden@gmail.com>
2025-05-29 14:57:44 +0300

    Fixed bugs in list creation

--- generateList.m
+++ generateList.m
@@ -28,6 +28,124 @@ elseif nargin < 4
     maxA = 0.1; %m^2
 end
 
+tmpnames = [   {'error'        }
+    {'satnum'       }
+    {'jdsatepoch'   }
+    {'ndot'         }
+    {'nddot'        }
+    {'bstar'        }
+    {'inclo'        }
+    {'nodeo'        }
+    {'ecco'         }
+    {'argpo'        }
+    {'mo'           }
+    {'no'           }
+    {'a'            }
+    {'alta'         }
+    {'altp'         }
+    {'isimp'        }
+    {'method'       }
+    {'aycof'        }
+    {'con41'        }
+    {'cc1'          }
+    {'cc4'          }
+    {'cc5'          }
+    {'d2'           }
+    {'d3'           }
+    {'d4'           }
+    {'delmo'        }
+    {'eta'          }
+    {'argpdot'      }
+    {'omgcof'       }
+    {'sinmao'       }
+    {'t'            }
+    {'t2cof'        }
+    {'t3cof'        }
+    {'t4cof'        }
+    {'t5cof'        }
+    {'x1mth2'       }
+    {'x7thm1'       }
+    {'mdot'         }
+    {'nodedot'      }
+    {'xlcof'        }
+    {'xmcof'        }
+    {'nodecf'       }
+    {'irez'         }
+    {'d2201'        }
+    {'d2211'        }
+    {'d3210'        }
+    {'d3222'        }
+    {'d4410'        }
+    {'d4422'        }
+    {'d5220'        }
+    {'d5232'        }
+    {'d5421'        }
+    {'d5433'        }
+    {'dedt'         }
+    {'del1'         }
+    {'del2'         }
+    {'del3'         }
+    {'didt'         }
+    {'dmdt'         }
+    {'dnodt'        }
+    {'domdt'        }
+    {'e3'           }
+    {'ee2'          }
+    {'peo'          }
+    {'pgho'         }
+    {'pho'          }
+    {'pinco'        }
+    {'plo'          }
+    {'se2'          }
+    {'se3'          }
+    {'sgh2'         }
+    {'sgh3'         }
+    {'sgh4'         }
+    {'sh2'          }
+    {'sh3'          }
+    {'si2'          }
+    {'si3'          }
+    {'sl2'          }
+    {'sl3'          }
+    {'sl4'          }
+    {'gsto'         }
+    {'xfact'        }
+    {'xgh2'         }
+    {'xgh3'         }
+    {'xgh4'         }
+    {'xh2'          }
+    {'xh3'          }
+    {'xi2'          }
+    {'xi3'          }
+    {'xl2'          }
+    {'xl3'          }
+    {'xl4'          }
+    {'xlamo'        }
+    {'zmol'         }
+    {'zmos'         }
+    {'atime'        }
+    {'xli'          }
+    {'xni'          }
+    {'operationmode'}
+    {'no_kozai'     }
+    {'am'           }
+    {'em'           }
+    {'im'           }
+    {'Om'           }
+    {'mm'           }
+    {'nm'           }
+    {'tumin'        }
+    {'mu'           }
+    {'radiusearthkm'}
+    {'xke'          }
+    {'j2'           }
+    {'j3'           }
+    {'j4'           }
+    {'j3oj2'        }
+    {'init'         }];
+tmpvalues = repmat({0},115,1);
+tmp = cell2struct(tmpvalues,tmpnames);
+
 GPdata = readstruct(filename);
 
 numberOfsats = length(GPdata.omm);
@@ -38,7 +156,7 @@ v = zeros(numberOfsats,3);
 m = zeros(numberOfsats,1);
 A = zeros(numberOfsats,1);
 epoch = NaT(numberOfsats,1);
-satrecList = repmat(struct('a',0),numberOfsats,1);
+satrecList = repmat(tmp,numberOfsats,1);
 
 numOfDecayed = 0;
 numOfStale = 0;
@@ -59,7 +177,7 @@ for i = 1:numberOfsats
     % Genetate the R and v
     satrec = GPxml2rv(whichconsts,'i',consts,satstructxml);
     ep_ = satstructxml.meanElements.EPOCH;
-    [~,r_,v_] = sgp4(satrec,0,consts);
+    [~,r_,v_] = sgp4(satrec,0);%,consts);
 
     %check if the data has RCS_SIZE
     RCSPlace = find(satParamsNames == "RCS_SIZE");
--- generateSatrecList.m
+++ generateSatrecList.m
@@ -13,13 +13,131 @@ function [satrecList] = generateSatrecList(filename, timefilecreated,maxdur, con
 %   propagation)
 %
 
+tmpnames = [   {'error'        }
+    {'satnum'       }
+    {'jdsatepoch'   }
+    {'ndot'         }
+    {'nddot'        }
+    {'bstar'        }
+    {'inclo'        }
+    {'nodeo'        }
+    {'ecco'         }
+    {'argpo'        }
+    {'mo'           }
+    {'no'           }
+    {'a'            }
+    {'alta'         }
+    {'altp'         }
+    {'isimp'        }
+    {'method'       }
+    {'aycof'        }
+    {'con41'        }
+    {'cc1'          }
+    {'cc4'          }
+    {'cc5'          }
+    {'d2'           }
+    {'d3'           }
+    {'d4'           }
+    {'delmo'        }
+    {'eta'          }
+    {'argpdot'      }
+    {'omgcof'       }
+    {'sinmao'       }
+    {'t'            }
+    {'t2cof'        }
+    {'t3cof'        }
+    {'t4cof'        }
+    {'t5cof'        }
+    {'x1mth2'       }
+    {'x7thm1'       }
+    {'mdot'         }
+    {'nodedot'      }
+    {'xlcof'        }
+    {'xmcof'        }
+    {'nodecf'       }
+    {'irez'         }
+    {'d2201'        }
+    {'d2211'        }
+    {'d3210'        }
+    {'d3222'        }
+    {'d4410'        }
+    {'d4422'        }
+    {'d5220'        }
+    {'d5232'        }
+    {'d5421'        }
+    {'d5433'        }
+    {'dedt'         }
+    {'del1'         }
+    {'del2'         }
+    {'del3'         }
+    {'didt'         }
+    {'dmdt'         }
+    {'dnodt'        }
+    {'domdt'        }
+    {'e3'           }
+    {'ee2'          }
+    {'peo'          }
+    {'pgho'         }
+    {'pho'          }
+    {'pinco'        }
+    {'plo'          }
+    {'se2'          }
+    {'se3'          }
+    {'sgh2'         }
+    {'sgh3'         }
+    {'sgh4'         }
+    {'sh2'          }
+    {'sh3'          }
+    {'si2'          }
+    {'si3'          }
+    {'sl2'          }
+    {'sl3'          }
+    {'sl4'          }
+    {'gsto'         }
+    {'xfact'        }
+    {'xgh2'         }
+    {'xgh3'         }
+    {'xgh4'         }
+    {'xh2'          }
+    {'xh3'          }
+    {'xi2'          }
+    {'xi3'          }
+    {'xl2'          }
+    {'xl3'          }
+    {'xl4'          }
+    {'xlamo'        }
+    {'zmol'         }
+    {'zmos'         }
+    {'atime'        }
+    {'xli'          }
+    {'xni'          }
+    {'operationmode'}
+    {'no_kozai'     }
+    {'am'           }
+    {'em'           }
+    {'im'           }
+    {'Om'           }
+    {'mm'           }
+    {'nm'           }
+    {'tumin'        }
+    {'mu'           }
+    {'radiusearthkm'}
+    {'xke'          }
+    {'j2'           }
+    {'j3'           }
+    {'j4'           }
+    {'j3oj2'        }
+    {'init'         }];
+tmpvalues = repmat({0},115,1);
+tmp = cell2struct(tmpvalues,tmpnames);
+
 GPdata = readstruct(filename);
 
 numberOfsats = length(GPdata.omm);
 
 %preallocate memory
 
-%satrecList = repmat(struct('a',0),numberOfsats,1);
+satrecList = repmat(tmp,numberOfsats,1);
 
 numOfDecayed = 0;
 numOfStale = 0;
@@ -44,5 +162,5 @@ for i = 1:numberOfsats
     listCount = listCount+1;
 end %end for
 %
-%satrecList = satrecList(1:end-numOfDecayed-numOfStale);
+satrecList = satrecList(1:end-numOfDecayed-numOfStale);
 end
